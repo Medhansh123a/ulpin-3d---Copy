e857327fcb6793db9f014834824e53513a4ce0e7
Medhansh <medhansh020209@gmail.com>
2026-09-04 17:49:01 +0530

    Fix navigation

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate ,type NavigationFunction} from 'react-router-dom';
 import { Fingerprint, Menu, Moon, Search, Sun } from 'lucide-react';
 import { useApp } from '../context/AppContext';
 import { findByUlpin, searchEntities } from '../data/mockData';
